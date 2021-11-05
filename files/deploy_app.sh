3d34e42a2581ff244764cda7b21d58f1c69bfdfc
Niharika-Jaikumar <niharikajk@gmail.com>
2021-11-05 17:49:11 +0530

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Pretty Cat.
+  Welcome to ${PREFIX}'s app. Catchy slogan cant think of right now:)
   <!-- END -->
 
   </div>
