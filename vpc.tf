1bf43c1ab868f13747127b6a12fbd69603021329
Niharika <niharikajk@gmail.com>
2021-11-05 13:16:05 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,10 +2,10 @@ module "network" {
   source  = "app.terraform.io/dbhashicorp/network/google"
   version = "3.4.0"
   subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
